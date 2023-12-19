25be0da196570cb7a6bf856730d6cf312de9a9c3
Faisal Herawy <faisalherawy@icloud.com>
2023-12-19 10:40:00 +0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Faisal Herawy on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
